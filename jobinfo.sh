412ce85551c237f58b351c04c1b23e2dd0d239dc
Aaron Kollasch <awkollasch@gmail.com>
2019-11-18 18:26:04 -0500

    Address shellcheck corrections

--- jobinfo.sh
+++ jobinfo.sh
@@ -10,8 +10,8 @@
 
 jobinfo() {
     # if no argument is given, then print info on all running or recently finished jobs
-    if [ $# -eq 0 ]; then
-        squeue -u $USER -t 'all'
+    if [[ $# -eq 0 ]]; then
+        squeue -u "$USER" -t 'all'
         return 0
     fi
 
@@ -19,11 +19,11 @@ jobinfo() {
     echo "Job Name: $(sacct -o jobname,timelimit -j $1 -n -P | head -n 1 | sed -e 's/|/\nTime Limit: /')"
 
     # seff prints efficiency and other info for the job
-    seff $1
+    seff "$1"
     echo
 
     # sacct prints a table of info on completed job steps
-    sacct -o jobid,alloccpus,state,reqmem,maxrss,averss,maxvmsize,elapsed -j $1
+    sacct -o jobid,alloccpus,state,reqmem,maxrss,averss,maxvmsize,elapsed -j "$1"
 
     # sstat prints info on currently running job steps
     # capture stdout and stderr to eliminate duplicate sstat check
@@ -31,7 +31,7 @@ jobinfo() {
 #    eval "$({ serr=$({ sout=$(sstat -o jobid,ntasks,avecpu,avecpufreq,maxrss,averss,maxvmsize,avevmsize -j $1); sret=$?; } 2>&1; declare -p sout sret >&2); declare -p serr; } 2>&1)"
     eval "$({
         SERR=$({
-            SOUT=$(sstat -o jobid,ntasks,avecpu,avecpufreq,maxrss,averss,maxvmsize,avevmsize -j $1)
+            SOUT=$(sstat -o jobid,ntasks,avecpu,avecpufreq,maxrss,averss,maxvmsize,avevmsize -j "$1")
             SRET=$?
         } 2>&1
         declare -p SOUT SRET >&2)
@@ -44,7 +44,7 @@ jobinfo() {
     fi
 
     # sprio prints priority info on jobs that have not yet started
-    SOUT=$(sprio -j $1)
+    SOUT=$(sprio -j "$1")
     if [[ ${SOUT} != "Unable to find jobs matching user/id(s) specified" ]]; then
         echo
         echo -e "$SOUT"
@@ -57,22 +57,22 @@ jobinfo() {
 _comp_jobinfo()
 {
     # complete with currently running jobs
-    _script_commands="$(squeue -u $USER -h -o '%i' | tr '\n' ' ')"
+    _script_commands="$(squeue -u "$USER" -h -t 'R,CG' -o '%i' | tr '\n' ' ')"
 
     # if there are no currently running jobs, complete with all recent jobs
-    if [ -z "$_script_commands" ]; then
-        _script_commands="$(squeue -u $USER -h -o '%i' -t 'all' -S '-e' | tr '\n' ' ')"
+    if [[ -z "$_script_commands" ]]; then
+        _script_commands="$(squeue -u "$USER" -h -o '%i' -t 'all' -S '-e' | tr '\n' ' ')"
     fi
 
     # otherwise, complete with nothing
-    if [ -z "$_script_commands" ]; then
+    if [[ -z "$_script_commands" ]]; then
         _script_commands=" "
     fi
 
     local cur
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
-    COMPREPLY=( $(compgen -W "${_script_commands}" -- ${cur}) )
+    COMPREPLY=( $(compgen -W "${_script_commands}" -- "${cur}") )
 
     return 0
 }
